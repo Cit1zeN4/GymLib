3bc660f40373a5f40b3aea6140acb499d6209479
Max <maximrimkus@yandex.by>
2023-12-05 17:31:17 +0300

    Added ApiContext.cs and ApiContextInitializer.cs

---
+++ GymLibAPI.Data/ApiContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GymLibAPI.Data;
+
+public class ApiContext : DbContext
+{
+    public ApiContext(DbContextOptions<ApiContext> options) : base(options) { }
+}
---
+++ GymLibAPI.Data/ApiContextInitializer.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace GymLibAPI.Data;
+
+public static class ApiContextInitializer 
+{
+    public static void Init(IServiceProvider provider)
+    {
+        using var context = provider.GetRequiredService<ApiContext>();
+    }
+}
